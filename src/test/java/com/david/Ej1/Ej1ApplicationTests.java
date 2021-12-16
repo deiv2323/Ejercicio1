6f796682c7c182bd4ab19eb29ac1ced6ea0f00be
david0206 <96118565+deiv2323@users.noreply.github.com>
2021-12-16 10:53:44 +0100

    confirmacion

---
+++ src/test/java/com/david/Ej1/Ej1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.david.Ej1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ej1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
